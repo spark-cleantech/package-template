fcd0182bcfa9b7ae1c1c90a215fc5e5d130763f9
Erwan Pannier <erwan.pannier@gmail.com>
2024-03-16 21:25:51 +0100

    update licence, and simplify

--- {{cookiecutter.project_name}}/docs/conf.py
+++ {{cookiecutter.project_name}}/docs/conf.py
@@ -9,17 +9,17 @@
 import os
 import sys
 
-import {{ cookiecutter.project_slug }}
+import {{ cookiecutter.repo_name }}
 
 sys.path.insert(0, os.path.abspath("../"))
 
 # -- Project information -----------------------------------------------------
 
-project = "{{ cookiecutter.project_slug }}"
-copyright = "2024, Copyright (C) Spark Cleantech SAS (SIREN 909736068)"
+project = "{{ cookiecutter.repo_name }}"
+copyright = " {{ cookiecutter.copyright_year }}, Copyright (C) Spark Cleantech SAS (SIREN 909736068)"
 author = "{{ cookiecutter.project_author }}"
-version = {{ cookiecutter.project_slug }}.__version__
-release = {{ cookiecutter.project_slug }}.__version__
+version = {{ cookiecutter.repo_name }}.__version__
+release = {{ cookiecutter.repo_name }}.__version__
 
 # -- General configuration ---------------------------------------------------
 
@@ -37,10 +37,8 @@ extensions = [
 autodoc_typehints = "none"
 
 # autoapi configuration
-autoapi_add_toctree_entry = False
-autoapi_dirs = ["../{{ cookiecutter.project_slug }}"]
+autoapi_dirs = ["../{{ cookiecutter.repo_name }}"]
 autoapi_ignore = ["*/version.py"]
-autoapi_member_order = "groupwise"
 autoapi_options = [
     "members",
     "inherited-members",
